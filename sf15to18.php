ac63d7825f96a807856feb2358acf3ddb58e1dc1
Markus Slabina <io@markus.codes>
2018-08-03 09:44:59 +0200

    fixed length-checks and some typos/comments

--- sf15to18.php
+++ sf15to18.php
@@ -1,38 +1,41 @@
 <?php
+//takes a 15 character case sensetive Id and turns it into an 18 character case insensitive Id
 function sfdc15to18($id)
-{//takes a 15 character case sensetive Id and turns it into an 18 character case insensative Id
-
+{
     $charLibrary = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ012345';
 
-    //check the id is 18 digets long
-    if(strlen($id) != 15)
-    {//it is, exit
-        return $id;
+    // check the id is 18 digits long
+    if(strlen($id) == 18)
+    {
+        return $id; //it is, exit
     }
 
-    //check the id is 15 digets long
+    // check if the id is 15 digits long
     if(strlen($id) != 15)
     {
-        throw new Exception("'id' must be exactly least 15 characters long");
+        throw new Exception("The given id isn't 15 characters long.");
     }
-    //generate the last 3 digets
+    
+    // generate the last 3 digits
     for($i = 0; $i < 3; $i++)
     {
         $charLookup = 0;
 
-        //we'll use 5 characters to generate the check digits
+        // For every 5-digit block of the given id
         for($j = 0; $j < 5; $j++)
         {
+            // Assign the j-th chracter of the i-th 5-digit block to c
             $charFound = substr($id, (($i * 5) + $j), 1);
 
-            //check if this character is uppercase
+            // check if this character is an uppercase letter
             if(!is_numeric($charFound) && strtoupper($charFound) === $charFound)
-            {//it is, do bitshift magic
+            {
+                // Set a 1 at the character's position in the reversed segment
                 $charLookup += 1 << $j;
             }
         }
 
-        //add our newly generated character to the string
+        // Add the calculated character for the current block to the id
         $id .= substr($charLibrary, $charLookup, 1);
     }
 
